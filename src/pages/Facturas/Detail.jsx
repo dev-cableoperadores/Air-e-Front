73fcc60ec36b2699e6332fad5603baf3aa92f71e
adrian G <adriangomezcortes04@gmail.com>
2025-12-17 16:23:16 -0500

    exposicion a red

--- src/pages/Facturas/Detail.jsx
+++ src/pages/Facturas/Detail.jsx
@@ -203,8 +203,8 @@ const FacturasDetail = () => {
       <div className="bg-white rounded-lg shadow-md p-6 space-y-6">
         {/* Encabezado */}
         <div className="bg-secondary text-white rounded-lg p-4 mb-6">
-          <h3 className="bg-black text-2xl font-bold text-center"> {factura.cableoperador?.nombre_largo || 'N/A'}</h3>
-          <p className="text-center mt-2 opacity-90">
+          <h3 className="text-2xl font-bold text-center"> {factura.cableoperador?.nombre_largo || 'N/A'}</h3>
+          <p className="text-center mt-2 opacity-90 text-black font-semibold">
             Factura N° {factura.Num_factura}
           
           </p>
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { toast } from 'react-hot-toast'
 
 const API_BASE_URL =  'http://127.0.0.1:8000/'
+//const API_BASE_URL =  'http://10.21.13.89:8000/'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
